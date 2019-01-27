639f6112f8fd45fa8005f55dcc84e031c488566f
Cyrus Behroozi <cyrus.behr@gmail.com>
2019-01-27 06:54:40 -0800

    Pre npm audit fix save

--- server/api.js
+++ server/api.js
@@ -3,7 +3,9 @@ const api = express();
 const mongoose = require('mongoose');
 const { Clinic, Hospital } = require('../models/models');
 
-api.post('/registerClinic', function(req, res) {});
+api.post('/registerClinic', function(req, res) {
+  req.checkBody('username', 'Username cannot be empty').noEmpty();
+});
 
 module.exports = {
   api,
