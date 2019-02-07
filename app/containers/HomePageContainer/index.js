500080bfb4362b7ce1f2053e2f3ae0517f2c69a9
Cyrus Behroozi <cyrus.behr@gmail.com>
2019-02-06 23:54:04 -0800

    Added transitions to home page componenets

--- app/containers/HomePageContainer/index.js
+++ app/containers/HomePageContainer/index.js
@@ -10,6 +10,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
 import UserSelectionCard from 'components/UserSelectionCard/index';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import injectReducer from 'utils/injectReducer';
 import { LoginCardData } from './constants';
 
@@ -18,30 +19,72 @@ import reducer from './reducer';
 
 /* eslint-disable react/prefer-stateless-function */
 export class HomePageContainer extends React.Component {
+  state = {
+    appear: false,
+  };
+
+  componentDidMount() {
+    this.setState({
+      appear: true,
+    });
+  }
+
   render() {
     return (
       <div className="homePageContainer">
         <div>
           <div className="align-text-center title-margin">
             <div className="cFontItalics cRed-Text">
-              <span className="lighter title-font-size">Quick</span>
-              <span className="bolder title-font-size">ER</span>
+              <TransitionGroup>
+                <CSSTransition
+                  in={this.state.appear}
+                  appear
+                  timeout={1200}
+                  children={
+                    <div>
+                      <span className="lighter title-font-size">Quick</span>
+                      <span className="bolder title-font-size">ER</span>
+                    </div>
+                  }
+                  classNames="fade"
+                />
+              </TransitionGroup>
             </div>
-            <div className="action-subtitle cFont cBlue-Text">
-              Choose your role
-            </div>
+            <TransitionGroup>
+              <CSSTransition
+                in={this.state.appear}
+                appear
+                timeout={2500}
+                children={
+                  <div className="action-subtitle cFont cBlue-Text">
+                    Choose your role
+                  </div>
+                }
+                classNames="fade2"
+              />
+            </TransitionGroup>
           </div>
         </div>
         <div className="login-card-contianer">
           {LoginCardData.map(data => {
             return (
-              <UserSelectionCard
-                key={data.title}
-                title={data.title}
-                redirectRoute={data.redirectRoute}
-                {...this.props}
-                onClickFunc={() => console.log('Remove this')}
-              />
+              <TransitionGroup key={data.title}>
+                <CSSTransition
+                  in={this.state.appear}
+                  appear
+                  timeout={40000}
+                  children={
+                    <UserSelectionCard
+                      key={data.title}
+                      title={data.title}
+                      redirectRoute={data.redirectRoute}
+                      {...this.props}
+                      onClickFunc={() => console.log('Remove this')}
+                    />
+                  }
+                  classNames="fade3"
+                />
+              </TransitionGroup>
             );
           })}
         </div>
